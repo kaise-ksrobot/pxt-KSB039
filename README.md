aad32cebb1a4960df0a19fb8cb0abf387457f7ea
arthur <arthur@kaise.com.tw>
2021-01-11 10:00:13 +0800

    v0.1.5

--- README.md
+++ README.md
@@ -1,31 +1,28 @@
+# KSB039 RJ11 Seneor Board
 
-> Open this page at [https://kaise-ksrobot.github.io/pxt-ksb039/](https://kaise-ksrobot.github.io/pxt-ksb039/)
+![image](images/ksb039.png)
 
-## Use as Extension
+P0  Beep\
+P1  Mic\
+P2  Slider\
+P13 Ultrasonic\
+P14 IR Remote\
+P16 RGB LED
 
-This repository can be added as an **extension** in MakeCode.
 
-* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
-* click on **New Project**
-* click on **Extensions** under the gearwheel menu
-* search for **https://github.com/kaise-ksrobot/pxt-ksb039** and import
 
-## Edit this project ![Build status badge](https://github.com/kaise-ksrobot/pxt-ksb039/workflows/MakeCode/badge.svg)
+## User Guide 
+![image](images/block.png)
 
-To edit this repository in MakeCode.
+## Code Example 
 
-* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
-* click on **Import** then click on **Import URL**
-* paste **https://github.com/kaise-ksrobot/pxt-ksb039** and click import
+## License
 
-## Blocks preview
+MIT
 
-This image shows the blocks code from the last commit in master.
-This image may take a few minutes to refresh.
-
-![A rendered view of the blocks](https://github.com/kaise-ksrobot/pxt-ksb039/raw/master/.github/makecode/blocks.png)
-
-#### Metadata (used for search, rendering)
+## Supported targets
 
 * for PXT/microbit
-<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
+(The metadata above is needed for package search.)
+
+
--- main.ts
+++ main.ts
@@ -1,1 +1,131 @@
+/**
+ * KSB039 V0.010
+ */
+//% weight=10 color=#00A6F0 icon="\uf085" block="KSB039"
+namespace KSB039 {
 
+    let neoStrip: neopixel.Strip;
+
+
+    /**
+    * P1
+    */
+    //% blockId=KSB039_Mic
+    //% block="Mic"
+    //% weight=100
+    export function Mic(): number {
+
+        return pins.analogReadPin(AnalogPin.P1);
+    }
+
+    /**
+    * P2
+    */
+    //% blockId=KSB039_Slider
+    //% block="Slider"
+    //% weight=99
+    export function Slider(): number {
+
+        return pins.analogReadPin(AnalogPin.P2);;
+    }
+
+
+    /**
+    * P13
+    */
+    //% blockId=KSB039_Ultrasonic
+    //% block="Ultrasonic(cm)"
+    //% weight=98
+    export function Ultrasonic(): number {
+
+        let maxCmDistance = 500
+        // send pulse
+        pins.setPull(DigitalPin.P13, PinPullMode.PullNone);
+        pins.digitalWritePin(DigitalPin.P13, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(DigitalPin.P13, 1);
+        control.waitMicros(10);
+        pins.digitalWritePin(DigitalPin.P13, 0);
+
+        const d = pins.pulseIn(DigitalPin.P13, PulseValue.High, maxCmDistance * 58);
+        // read pulse
+
+        return Math.idiv(d, 58);
+    }
+
+    
+
+    /**
+    * P16
+    */
+    //% blockId="KSB039_RGB1" 
+    //% block="RGB LED1"
+    //% weight=96
+    export function RGB_LED1(): neopixel.Strip {
+        if (!neoStrip) {
+            neoStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+
+        }
+
+        return neoStrip.range(0, 1);
+    }
+    /**
+    * P16
+    */
+    //% blockId="KSB039_RGB2" 
+    //% block="RGB LED2"
+    //% weight=95
+    export function RGB_LED2(): neopixel.Strip {
+        if (!neoStrip) {
+            neoStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+
+        }
+
+        return neoStrip.range(1, 1);
+    }
+    /**
+    * P16
+    */
+    //% blockId="KSB039_RGB3" 
+    //% block="RGB LED3"
+    //% weight=94
+    export function RGB_LED3(): neopixel.Strip {
+        if (!neoStrip) {
+            neoStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+
+        }
+
+        return neoStrip.range(2, 1);
+    }
+    /**
+    * P16
+    */
+    //% blockId="KSB039_RGB4" 
+    //% block="RGB LED4"
+    //% weight=93
+    export function RGB_LED4(): neopixel.Strip {
+        if (!neoStrip) {
+            neoStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+
+        }
+
+        return neoStrip.range(3, 1);
+    }
+    /**
+    * P16
+    */
+    //% blockId="KSB039_RGB" 
+    //% block="RGB LED Setting"
+    //% weight=92
+    export function RGB_LED(): neopixel.Strip {
+        if (!neoStrip) {
+            neoStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+
+        }
+
+        return neoStrip;
+    }
+
+
+
+}
